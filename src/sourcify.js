b52cd57458b13d4a3a3b7940464f57318b9784d1
numtel <ben@latenightsketches.com>
2024-07-10 02:27:25 -0700

    Load sourcify sources directly from sourcify, not ipfs

--- src/sourcify.js
+++ src/sourcify.js
@@ -1,10 +1,8 @@
 import { getAddress } from 'viem';
 
-const IPFS_PREFIX = 'dweb:/ipfs/';
-
 export default async function(contractAddress, chainId = 1, matchType = 'full_match') {
-  const baseUrl = `https://repo.sourcify.dev/contracts/${matchType}/${chainId}/${getAddress(contractAddress)}/metadata.json`;
-  const response = await fetch(baseUrl);
+  const baseUrl = `https://repo.sourcify.dev/contracts/${matchType}/${chainId}/${getAddress(contractAddress)}/`;
+  const response = await fetch(baseUrl + 'metadata.json');
   
   // Check if the response is successful
   if (!response.ok) {
@@ -18,13 +16,9 @@ export default async function(contractAddress, chainId = 1, matchType = 'full_ma
   // Extract/load the Solidity source code
   const soliditySource = data.sources;
   for(let file of Object.keys(soliditySource)) {
-    if(!('content' in soliditySource[file]) && soliditySource[file].urls instanceof Array) {
-      const ipfsUrl = soliditySource[file].urls.find(x => String(x).startsWith(IPFS_PREFIX));
-      const ipfsData = await fetch(
-        `${process.env.IPFS_GATEWAY || 'https://ipfs.io/ipfs/'}${ipfsUrl.slice(IPFS_PREFIX.length)}`
-      );
-      soliditySource[file].content = await ipfsData.text();
-
+    if(!('content' in soliditySource[file])) {
+      const fileResponse = await fetch(baseUrl + 'sources/' + file);
+      soliditySource[file].content = await fileResponse.text();
     }
   }
 
